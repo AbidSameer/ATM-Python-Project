20b1d4e7f7dee29a28edbe56e8af72551b063630
AbidSameer <149421607+AbidSameer@users.noreply.github.com>
2024-01-31 10:25:21 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,10 +1,15 @@
-
+import time
 username='sameer'
 password='abid123'
-
 c_username=input("Enter username ")
 c_password=input("Enter password ")
-
+n=1
+max_n=3
+time=24*60*60
+while(password!=c_password and n<max_n):
+    print("please try again")
+    c_password=input("Enter password ")
+    n+=1
 if(c_username==username and c_password==password):
     print('''
     1.deposit
@@ -32,4 +37,16 @@ if(c_username==username and c_password==password):
         exit()
     else:
         print("Please enter correct logins")
+else:
+    print("Please try after 24 hours")
+    for i in range(time,0,-1):
+        hours, remainder = divmod(i, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        time_format = "{:02}:{:02}:{:02}".format(hours, minutes, seconds)
+        print(f"Next attempt available in {time_format}", end="\r")
+
+        
+        
+    
+
     
